12c1e16dff9101cdc248ef3e30375fe51bd65119
huonggiangtran <huonggiangtran2003@gmail.com>
2024-06-20 16:23:38 +0700

    Initial Commit

---
+++ DemoChatApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoChatApp
+//
+//  Created by Lily Tran on 20/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
